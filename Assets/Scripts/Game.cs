0dbd7309fc7660cd559c3d2ca3f8411194071c8c
Noel Mateo <lasvegascoderschool@gmail.com>
2020-03-27 10:59:58 -0700

    Ronin

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int start = 0; start < 10; start += 1)
+        {
+            Instantiate(tile);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
